70aa6e53a025913a1aa2517c50a42fa507ac1c42
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-04 23:15:18 +0100

    Commit 1
    Inizio progetto

---
+++ lista-spesa-quattro-bis-server/src/test/java/it/iad2/listaspesaquattrobis/ListaspesaquattrobisApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad2.listaspesaquattrobis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaspesaquattrobisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
